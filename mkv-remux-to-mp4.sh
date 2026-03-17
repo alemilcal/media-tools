b719aba47f05de24a5bdf7e3bc79962a5170219e
alemilcal <alemilcal@gmail.com>
2026-03-17 19:06:55 +0100

    17/03/2026 19:06:55

---
+++ mkv-remux-to-mp4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Comprobar si se ha pasado un archivo
+if [ -z "$1" ]; then
+    echo "Uso: $0 archivo.mkv"
+    exit 1
+fi
+
+INPUT="$1"
+# Cambia la extensión .mkv por .mp4 para el nombre de salida
+OUTPUT="${INPUT%.mkv}.mp4"
+
+ffmpeg -i "$INPUT" \
+  -c copy \
+  -map 0:v -map 0:a -sn \
+  -movflags +faststart \
+  "$OUTPUT"
+
+echo "Proceso finalizado: $OUTPUT"

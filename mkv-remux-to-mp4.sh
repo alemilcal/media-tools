78c0156cb9f0af146bf301a0da1adf7f43c0981c
alemilcal <alemilcal@gmail.com>
2026-03-18 14:34:38 +0100

    18/03/2026 14:34:38

--- mkv-remux-to-mp4.sh
+++ mkv-remux-to-mp4.sh
@@ -8,17 +8,18 @@ fi
 
 INPUT="$1"
 # Cambia la extensión .mkv por .mp4 para el nombre de salida
+OUTPUTTMP="${INPUT%.mkv}.tmp.mp4"
 OUTPUT="${INPUT%.mkv}.mp4"
 
 nice ffmpeg -n -i "$INPUT" \
   -c copy \
   -map 0:v -map 0:a -sn \
   -movflags +faststart \
-  "$OUTPUT"
+  "$OUTPUTTMP"
 
 # $? captura el código de salida del último comando (ffmpeg)
 if [ $? -eq 0 ]; then
-    echo "Conversión exitosa: $OUTPUT"
+    echo "Conversión exitosa: $OUTPUTTMP"
 
     # Renombrar el original a .mkv.bak
     if [ ! -f "${INPUT}.bak" ]; then
@@ -27,6 +28,10 @@ if [ $? -eq 0 ]; then
     else
         echo "Aviso: El archivo de respaldo ${INPUT}.bak ya existe. No se ha renombrado."
     fi
+
+    mv "$OUTPUTTMP" "$OUTPUT"
+    echo "${OUTPUTTMP} renombrado a ${OUTPUT}"
+
 else
     echo "Error: ffmpeg falló o el archivo de salida ya existía. No se ha creado el respaldo."
     exit 1
